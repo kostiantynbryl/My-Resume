bdda8a8df32810dd1fc22358a532cfa74b63499e
Kostiantyn Bryl <kostiantynbryl@gmail.com>
2026-08-14 02:45:18 +0300

    fix: load final portfolio modules in order

--- final-v3.js
+++ final-v3.js
@@ -1,2 +1,2 @@
 /* NORVEXA final portfolio layer */
-['v3/ecosystem.js','v3/interactions.js'].forEach(src=>{const s=document.createElement('script');s.src=src;s.defer=true;document.head.appendChild(s)});
+['v3/data.js','v3/render.js','v3/interactions.js'].forEach(src=>{const s=document.createElement('script');s.src=src;s.async=false;document.head.appendChild(s)});
