0c8d13990fd45bb0146ccd86ce3c22a783011609
Kostiantyn Bryl <kostiantynbryl@gmail.com>
2026-08-14 02:43:28 +0300

    feat: add final portfolio loader

---
+++ final-v3.js
@@ -0,0 +1,2 @@
+/* NORVEXA final portfolio layer */
+['v3/ecosystem.js','v3/interactions.js'].forEach(src=>{const s=document.createElement('script');s.src=src;s.defer=true;document.head.appendChild(s)});
