b9c30ac888c360edf8301691bab852aa105c7506
Kostiantyn Bryl <kostiantynbryl@gmail.com>
2026-08-14 03:01:00 +0300

    ci: add Lighthouse quality thresholds

---
+++ lighthouserc.cjs
@@ -0,0 +1,1 @@
+module.exports={ci:{collect:{staticDistDir:'./',numberOfRuns:1,url:['http://localhost/']},assert:{assertions:{'categories:performance':['error',{minScore:0.95}],'categories:accessibility':['error',{minScore:0.95}],'categories:best-practices':['error',{minScore:0.95}],'categories:seo':['error',{minScore:0.95}]}},upload:{target:'filesystem',outputDir:'.lighthouseci-reports'}}};
